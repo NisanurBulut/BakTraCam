e74401641bde265ad16a475b1578e810ceeec2cf
NisanurBulut <nisanurrunasin@gmail.com>
2020-09-07 23:23:30 +0300

    bakım model guncellendı.

--- SERVICE/BakTraCam.Service.DataContract/BakimModel.cs
+++ SERVICE/BakTraCam.Service.DataContract/BakimModel.cs
@@ -6,5 +6,14 @@ namespace BakTraCam.Service.DataContract
     {
         public int Id { get; set; }
         public string Aciklama { get; set; }
+        public string Ad { get; set; }
+        public DateTime Tarihi { get; set; }
+        public string Gerceklestiren1 { get; set; }
+        public string Gerceklestiren2 { get; set; }
+        public string Gerceklestiren3 { get; set; }
+        public string Gerceklestiren4 { get; set; }
+        public string Sorumlu1 { get; set; }
+        public string Sorumlu2 { get; set; }
+        public int Oncelik { get; set; }
     }
 }
