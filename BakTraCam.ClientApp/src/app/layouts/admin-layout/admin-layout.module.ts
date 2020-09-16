8826c93b65d75324232e7b16a353676718468ec7
NisanurBulut <nisanurrunasin@gmail.com>
2020-09-16 17:38:40 +0300

    html düzenlemesi yapıldı.

--- BakTraCam.ClientApp/src/app/layouts/admin-layout/admin-layout.module.ts
+++ BakTraCam.ClientApp/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -5,12 +5,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatRippleModule} from '@angular/material/core';
@@ -40,13 +34,7 @@ import {TranslateModule} from '@ngx-translate/core';
   ],
   declarations: [
     DashboardComponent,
-    UserProfileComponent,
-    TableListComponent,
-    TypographyComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
-    UpgradeComponent
+    UserProfileComponent
   ]
 })
 
--- BakTraCam.ClientApp/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ BakTraCam.ClientApp/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,13 +1,6 @@
 import { Routes } from '@angular/router';
-
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
 import { HomeComponent } from 'app/home/home.component';
 import { KullaniciListComponent } from 'app/kullanici/list/kullanici-list.component';
 import { BakimListComponent } from 'app/bakim';
@@ -25,22 +18,10 @@ export const AdminLayoutRoutes: Routes = [
     //   component: UserProfileComponent
     // }]
     // }, {
-    //   path: '',
-    //   children: [ {
-    //     path: 'icons',
-    //     component: IconsComponent
-    //     }]
-    // }, {
     //     path: '',
     //     children: [ {
     //         path: 'notifications',
     //         component: NotificationsComponent
-    //     }]
-    // }, {
-    //     path: '',
-    //     children: [ {
-    //         path: 'maps',
-    //         component: MapsComponent
     //     }]
     // }, {
     //     path: '',
@@ -60,10 +41,4 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'kullanici',      component: KullaniciListComponent },
     { path: 'dashboard',      component: DashboardComponent },
     { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'table-list',     component: TableListComponent },
-    { path: 'typography',     component: TypographyComponent },
-    { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent },
-    { path: 'notifications',  component: NotificationsComponent },
-    { path: 'upgrade',        component: UpgradeComponent },
 ];
