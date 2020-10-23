d80dc0c998c838dde02308de07b8e4c10fceea1d
NisanurBulut <nisanurrunasin@gmail.com>
2020-10-23 17:44:32 +0300

    configuration yazımına başlandı

---
+++ CORE/BakTraCam.Core.DataAccess/Configurations/BakimConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using BakTraCam.Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BakTraCam.Core.DataAccess.Configurations
+{
+    public class BakimConfiguration : IEntityTypeConfiguration<BakimEntity>
+    {
+        public void Configure(EntityTypeBuilder<BakimEntity> builder)
+        {
+            builder.ToTable("tBakim","dbo");
+            builder.HasKey(a=>a.Id);
+        }
+    }
+}
